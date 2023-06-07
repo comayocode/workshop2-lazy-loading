5d4d0c71e12b5dc54747b765b8006994d3e6a417
Comayocode <ronalc726@gmail.com>
2023-06-07 12:59:40 -0500

    feat: Conexion API, eventos add y remove img

--- src/index.js
+++ src/index.js
@@ -1,6 +1,33 @@
-/**
- * This file is just a silly example to show everything working in the browser.
- * When you're ready to start on your site, clear the file. Happy hacking!
- **/
+const urlBase = "https://randomfox.ca/images/";
+const btnAdd = document.querySelector('.add');
+const btnClear = document.querySelector('.clean');
 
-console.log('Happy hacking :)')
+btnAdd.addEventListener('click', () => {
+  var numeroImg = Math.floor(Math.random() * (123 - 1 + 1)) + 1
+
+  console.log(numeroImg);
+
+  const createImageNode = () => {
+    const imagen = document.createElement('img');
+    imagen.className = "imagen mx-auto mt-4";
+    imagen.width = "320";
+    imagen.src = `${urlBase}${numeroImg}.jpg`;
+
+    // console.log(imagen);
+    return imagen;
+  };
+
+  const createImage = createImageNode();
+  const contenedor = document.querySelector('#container-images');
+  contenedor.appendChild(createImage);
+});
+
+btnClear.addEventListener('click', () => {
+  const imagen = document.querySelector('.imagen');
+  const contenedor = document.querySelector('#container-images');
+  contenedor.removeChild(imagen)
+})
+
+
+
+
