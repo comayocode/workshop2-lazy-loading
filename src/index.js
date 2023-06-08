232bf0d1643e558dad8dec7b8e4e197db26990d0
Comayocode <ronalc726@gmail.com>
2023-06-08 09:52:49 -0500

    refactor: Organizar codigo en funciones

--- src/index.js
+++ src/index.js
@@ -2,31 +2,46 @@ const urlBase = "https://randomfox.ca/images/";
 const btnAdd = document.querySelector('.add');
 const btnClear = document.querySelector('.clean');
 
-btnAdd.addEventListener('click', () => {
+// Generar número random entre el 1 - 123
+const randomImg = () => {
   var numeroImg = Math.floor(Math.random() * (123 - 1 + 1)) + 1
+  return numeroImg
+}
 
-  console.log(numeroImg);
+// Crear nodo <img> agregando clases, la url de la imagen a renderizar y añadirla a un div padre
+const createImageNode = () => {
+  const imagen = document.createElement('img');
+  imagen.className = "imagen mx-auto";
+  imagen.width = "320";
+  imagen.src = `${urlBase}${randomImg()}.jpg`;
 
-  const createImageNode = () => {
-    const imagen = document.createElement('img');
-    imagen.className = "imagen mx-auto mt-4";
-    imagen.width = "320";
-    imagen.src = `${urlBase}${numeroImg}.jpg`;
+  const divImg = document.createElement('div');
+  divImg.className = "p-3";
+  divImg.appendChild(imagen);
 
-    // console.log(imagen);
-    return imagen;
-  };
+  // console.log(imagen);
+  return divImg;
+};
 
-  const createImage = createImageNode();
+// Añadir addImageNode
+const addImageNode = () => {
   const contenedor = document.querySelector('#container-images');
-  contenedor.appendChild(createImage);
-});
+  const createImg = createImageNode();
+  contenedor.append(createImg);
+}
 
-btnClear.addEventListener('click', () => {
-  const imagen = document.querySelector('.imagen');
+// Agregar evento click al botón añadir para agregar el nodo de imagen
+btnAdd.addEventListener('click', addImageNode)
+
+//Eliminar la primera imagen del DOM
+const removeImageNode = () => {
   const contenedor = document.querySelector('#container-images');
-  contenedor.removeChild(imagen)
-})
+  const divImg = document.querySelector('#container-images>div');
+  contenedor.removeChild(divImg);
+}
+
+// Agregar evento click al botón añadir para eliminar el nodo de imagen
+btnClear.addEventListener('click', removeImageNode);
 
 
 
