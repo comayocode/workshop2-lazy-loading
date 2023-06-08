a71964f0e42f6ca95402ee185d3986d8eb285b78
Comayocode <ronalc726@gmail.com>
2023-06-08 14:13:16 -0500

    feat: Añadir log, eliminar todas las imagenes, efecto loading
    - Log contando las imagenes añadidas y cargadas
    - Eliminar todas las imagenes a la vez
    - Efecto de tarjeta loading antes de cargar toda la imagenes

--- src/index.js
+++ src/index.js
@@ -14,7 +14,9 @@ const randomImg = () => {
 const createImageNode = () => {
   const imagen = document.createElement('img');
   imagen.className = "imagen mx-auto";
-  imagen.width = "320";
+  imagen.width = 320;
+  // Crear imagen SVG (se reemplaza cuando una imagen se carga por completo)
+  imagen.src = "data:image/svg+xml;base64,PHN2ZyB3aWR0aD0iMzIwIiBoZWlnaHQ9IjMyMCIgeG1sbnM9Imh0dHA6Ly93d3cudzMub3JnLzIwMDAvc3ZnIiB2ZXJzaW9uPSIxLjEiLz4=";
   imagen.dataset.src = `${urlBase}${randomImg()}.jpg`;
 
   const divImg = document.createElement('div');
@@ -36,11 +38,10 @@ const addImageNode = () => {
 // Agregar evento click al botón añadir para agregar el nodo de imagen
 btnAdd.addEventListener('click', addImageNode)
 
-//Eliminar la primera imagen del DOM
+//Eliminar todos los nodos hijos del contenedor de imagenes
 const removeImageNode = () => {
   const contenedor = document.querySelector('#container-images');
-  const divImg = document.querySelector('#container-images>div');
-  contenedor.removeChild(divImg);
+  contenedor.innerHTML = ""; //Elimina todos los nodos
 }
 
 // Agregar evento click al botón añadir para eliminar el nodo de imagen
--- src/lazy.js
+++ src/lazy.js
@@ -1,3 +1,6 @@
+// Contador para el log
+let totalImages = 0;
+let loadedImages = 0;
 
 // Verificar si la imagen es visible o no
 const isIntersecting = (entry) => {
@@ -5,11 +8,16 @@ const isIntersecting = (entry) => {
 }
 
 // Desregistrar o dejar de escuchar una imagen que ya se ha cargado
-const accion = (entry) => {
+const loadImage = (entry) => {
   const divContainerImg = entry.target; //Div padre de la imagen
   const image = divContainerImg.firstChild; //Obtener la imagen
   const url = image.dataset.src; //Obtener la url del dataset
   image.src = url; //Cargar la imagen
+
+  image.onload = () => {
+    loadedImages += 1; //Agregar +1 imagen ya cargada
+    log(); // Ejecutar log
+  }
 
   // Cargar imagen una única vez. "Desregisrar" la imagen (unlisten)
   observer.unobserve(divContainerImg);
@@ -17,10 +25,18 @@ const accion = (entry) => {
 
 // Añadir imagen como intersectada o "ya fue visualizada en la pantalla"
 const observer = new IntersectionObserver(((allElements) => {
-  allElements.filter(isIntersecting).forEach(accion);
+  allElements.filter(isIntersecting).forEach(loadImage);
 }))
 
 // Registrar las imágenes
 export const registerImage = (imagen) => {
   observer.observe(imagen);
+  totalImages += 1; // Añadir +1 imagen cada que se da clic al botón añadir
+  log(); // Ejecutar log
+}
+
+function log() {
+  console.log(`⚪️ Total Imágenes: ${totalImages}`);
+  console.log(`🟣 Imágenes cargadas: ${loadedImages}`);
+  console.log("--------------------------------------");
 }
